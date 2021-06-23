b99362e112e5ad80098db59b8de5c0d26321d70c
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-06-23 17:53:22 -0500

    add lat long address to schema. copy in RESTful routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,46 @@
 class ContactsController < ApplicationController
-  def show_contact
-    render json: { message: Contact.all }
+  def index
+    render json: Contact.all
+  end
+
+  def show
+    id = params[:id]
+    contact = Contact.find_by(id: id)
+    render json: contact
+  end
+
+  def create
+    results = Geocoder.search("Paris")
+    results.first.coordinates
+
+    contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      address: params[:address],
+      latitude: params[:latitude],
+      longitude: params[:longitude],
+    )
+    contact.save
+    render json: contact
+  end
+
+  def update
+    the_id = params[:id]
+    contact = Contact.find_by(id: the_id)
+    contact.first_name = params[:first_name] || contact.first_name
+    contact.last_name = params[:last_name] || contact.last_name
+    contact.email = params[:email] || contact.email
+    contact.phone_number = params[:phone_number] || contact.phone_number
+    contact.save
+    render json: contact
+  end
+
+  def destroy
+    the_id = params[:id]
+    contact = Contact.find_by(id: the_id)
+    contact.destroy
+    render json: { message: "contact has been deleted" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/contact", controller: "contacts", action: "show_contact"
+  get "/contacts", controller: "contacts", action: "index"
+  get "/contacts/:id", controller: "contacts", action: "show"
+  post "/contacts/", controller: "contacts", action: "create"
+  patch "/contacts/:id", controller: "contacts", action: "update"
+  delete "/contacts/:id", controller: "contacts", action: "destroy"
 end
